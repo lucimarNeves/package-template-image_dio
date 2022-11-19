e91b1b04d4a48cf0a95b4e78e3cf6b47dabf7b12
Lucimar <lucimar.neves@gmail.com>
2022-11-19 01:13:45 -0300

    Update plot.py

--- processamento_de_imagens/utilitarios/plot.py
+++ processamento_de_imagens/utilitarios/plot.py
@@ -1,29 +1,28 @@
 import matplotlib.pyplot as plt
 
-
-def plotar_imagem(imagem):
-    plt.figure(figsize = (12, 4))
-    plt.imshow(imagem, cmap = 'gray')
+def plot_image(image):
+    plt.figure(figsize=(12, 4))
+    plt.imshow(image, cmap='gray')
     plt.axis('off')
     plt.show()
 
-def plotar_resultados(*args):
-    numero_imagens = len(args)
-    fig, axis = plt.subplots(nrows = 1, ncols = numero_imagens, figsize = (12, 4))
-    nomes_lst = ['image {}'.format(i) for i in range(1, numero_imagens)]
-    nomes_lst.append('Resultado')
-    for ax, nome, imagem in zip(axis, nomes_lst, args):
-        ax.set_title(nome)
-        ax.inshow(imagem, cmap='gray')
+def plot_result(*args):
+    numbers_images = len(args)
+    fig, axis = plt.subplots(nrows=1, ncols=numbers_images, figsize=(12, 4))
+    names_lst = ["Image{}".format(i) for i in range(1, numbers_images)]
+    names_lst.append('Result')
+    for ax, name, image, in zip(axis, names_lst, args):
+        ax.set_title(name)
+        ax.imshow(image, cmap='gray')
         ax.axis('off')
-    fig.tight_layout()
+    fig.light_layout()
     plt.show()
 
-def plotar_histograma(imagem):
-    fig, axis = plt.subplots(nrows = 1, ncols = 3, figsize = (12, 4), sharex = True, sharey = True)
-    cor_lst = ['red', 'green', 'blue']
-    for index, (ax, cor) in enumerate(zip(axis, cor_lst)):
-        ax.set_title('{} histogram'.format(cor.title()))
-        ax.hist(imagem[:, :, index].ravel(), bins = 256, cor = cor, alpha = 0.8)
+def plot_histogram(image):
+    fig, axis = plt.subplots(nrows=1, ncols=3, figsize=(12, 4), sharex=True, sharey=True)
+    color_lst = ['red', 'gren', 'blue']
+    for index, (ax, color) in enumerate(zip(axis, color_lst)):
+        ax.set_title('() histogram'.format(color.title()))
+        ax.hist(image[:, :, index].ravel(), bins= 256, color = color, aplha = 0.8)
     fig.tight_layout()
     plt.show()
